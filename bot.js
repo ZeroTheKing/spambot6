0d6fefb8d2a4aada820dc4ef350f35b93a39404d
ZeroTheKing <44266197+ZeroTheKing@users.noreply.github.com>
2018-11-07 19:59:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "1";
+var prefix = "6";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "509750934054633502"; // ايدي السررفر
+var channel = "509750934054633504";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client.guilds.get(server).channels.get(channel).send('**Ayman , Ayman , Ayman , Ayman , Ayman , Ayman , Ayman , Ayman , Ayman , Ayman , Ayman , Ayman , Ayman , Ayman , Ayman , Ayman , Ayman , Ayman , **')
     },305);
 })
 
